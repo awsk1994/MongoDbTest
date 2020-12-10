2123ac2e23fdcb50d65fd5b25f8cc5b6573ceb6e
Alex Wong <awsk1994@gmail.com>
2020-12-09 19:01:38 -0500

    Combine queries and use await to execute queries in order.

--- pa5.js
+++ pa5.js
@@ -1,45 +1,81 @@
-const mongo = require('mongodb');           // TODO: npm install mongodb??
+const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const url = 'mongodb://localhost:27017';
 
-// Helper Function
-function queryFn(dbName, tableName, fn){
-    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-        if (err) throw err;
-        const dbo = client.db(dbName)
-        const r = dbo.collection(tableName);
-        fn(r, client);
-    });
-}
+async function execute(){
+  const client = await MongoClient.connect(url, { useNewUrlParser: true })
+    .catch(err => { console.log(err); });
+  if (!client) {
+    return;
+  }
+
+  try {
+    const db = client.db("test");
+    let collection = db.collection('restaurants');
+    let cursor = await q1(collection);
+    console.log("q1:");
+    console.log(cursor);
+    
+    cursor = await q2(collection);
+    console.log("q2:");
+    console.log(cursor);
+
+    cursor = await q3(collection);
+    console.log("q3:");
+    console.log(cursor);
+
+    cursor = await q4(collection);
+    console.log("q4:");
+    console.log(cursor);
+
+    cursor = await q5(collection);
+    console.log("q5:");
+    console.log(cursor);
+
+    cursor = await q6(collection);
+    console.log("q6:");
+    console.log(cursor);
+
+    collection = db.collection('zips');
+    cursor = await q7(collection);
+    console.log("q7:");
+    console.log(cursor);
+    
+    cursor = await q8(collection);
+    console.log("q8:");
+    console.log(cursor);
+
+    cursor = await q9(collection);
+    console.log("q9:");
+    console.log(cursor);
+
+    cursor = await q10(collection);
+    console.log("q10:");
+    console.log(cursor);
+  } catch (err) {
+      console.err(err);
+  } finally {
+      client.close();
+  }
+};
+execute()
 
 // Queries
-function q1(r, client){
-    r.findOne({name: "Caffe Dante"}, {restaurant_id: 1}, function(err, result){
-        if(err) throw err;
-        console.log(result);
-        client.close();
-    });
+function q1(r){
+  return r.findOne({name: "Caffe Dante"}, {restaurant_id: 1});
 }
 
-function q2(r, client){
-    r.find({name: /.*Steak.*/}, {projection: {_id: 0, restaurant_id: 1, name: 1}})
-    .toArray(function(err, result){
-        if(err) throw err;
-        console.log(result);
-        client.close();
-    });
+function q2(r){
+  return r.find({name: /.*Steak.*/}, {projection: {_id: 0, restaurant_id: 1, name: 1}})
+    .toArray();
 }
 
-function q3(r, client){
-    r.find({cuisine: {$in: ["Italian", "American"]}, borough: "Brooklyn"}, {projection: {_id: 0, name: 1}})
-        .toArray(function(err, result){
-            if(err) throw err;
-            console.log(result);
-            client.close();
-        });
+function q3(r){
+  return r.find({cuisine: {$in: ["Italian", "American"]}, borough: "Brooklyn"}, {projection: {_id: 0, name: 1}})
+    .toArray();
 }
 
-function q4(r, client){
+function q4(r){
     let query = [
     {
         "$match" : {
@@ -66,124 +102,108 @@ function q4(r, client){
         }
     }];
 
-    r.aggregate(query).toArray(function(err, result){
-        if(err) throw err;
-        console.log(result);
-        client.close();
-    });
+    return r.aggregate(query).toArray();
 }
 
-function q5(r, client){
-    let query = [
-	{
-		"$match" : {
-			"cuisine" : /.*Chinese.*/,
-			"borough" : /.*Manhattan.*/
-		}
-	},
-	{
-		"$unwind" : "$grades"
-	},
-	{
-		"$unwind" : "$grades.score"
-	},
-	{
-		"$group" : {
-			"_id" : "$_id",
-			"totalScore" : {
-				"$sum" : "$grades.score"
-			},
-			"name" : {
-				"$first" : "$name"
-			}
-		}
-	},
-	{
-		"$project" : {
-			"name" : 1,
-			"totalScore" : 1
-		}
-	},
-	{
-		"$sort" : {
-			"totalScore" : -1
-		}
-	},
-	{
-		"$limit" : 5
-	}];
-    r.aggregate(query).toArray(function(err, result){
-        if(err) throw err;
-        console.log(result);
-        client.close();
-    });
-}
-
-function q6(r, client){
-    let query = [
-        {
-            "$unwind" : "$grades"
+function q5(r){
+  let query = [
+    {
+      "$match" : {
+        "cuisine" : /.*Chinese.*/,
+        "borough" : /.*Manhattan.*/
+      }
+    },
+    {
+      "$unwind" : "$grades"
+    },
+    {
+      "$unwind" : "$grades.score"
+    },
+    {
+      "$group" : {
+        "_id" : "$_id",
+        "totalScore" : {
+          "$sum" : "$grades.score"
         },
-        {
-            "$unwind" : "$grades.score"
-        },
-        {
-            "$match" : {
-                "$and" : [
-                    {
-                        "address.coord" : {
-                            "$geoWithin" : {
-                                "$box" : [
-                                    [
-                                        -74,
-                                        40.5
-                                    ],
-                                    [
-                                        -73.5,
-                                        40.7
-                                    ]
-                                ]
-                            }
-                        }
-                    },
-                    {
-                        "grades.score" : {
-                            "$gte" : 70
-                        }
-                    }
-                ]
-            }
-        },
-        {
-            "$group" : {
-                "_id" : "$_id"
-            }
-        },
-        {
-            "$project" : {
-                "_id" : 1,
-                "name" : 1
-            }
+        "name" : {
+          "$first" : "$name"
         }
-    ];
-    r.aggregate(query).toArray(function(err, result){
-        if(err) throw err;
-        console.log(result.length);
-        client.close();
-    });
+      }
+    },
+    {
+      "$project" : {
+        "name" : 1,
+        "totalScore" : 1
+      }
+    },
+    {
+      "$sort" : {
+        "totalScore" : -1
+      }
+    },
+    {
+      "$limit" : 5
+    }];
+  return r.aggregate(query).toArray();
 }
 
-function q7(r, client){
+function q6(r){
+let query = [
+      {
+          "$unwind" : "$grades"
+      },
+      {
+          "$unwind" : "$grades.score"
+      },
+      {
+          "$match" : {
+              "$and" : [
+                  {
+                      "address.coord" : {
+                          "$geoWithin" : {
+                              "$box" : [
+                                  [
+                                      -74,
+                                      40.5
+                                  ],
+                                  [
+                                      -73.5,
+                                      40.7
+                                  ]
+                              ]
+                          }
+                      }
+                  },
+                  {
+                      "grades.score" : {
+                          "$gte" : 70
+                      }
+                  }
+              ]
+          }
+      },
+      {
+          "$group" : {
+              "_id" : "$_id"
+          }
+      },
+      {
+          "$project" : {
+              "_id" : 1,
+              "name" : 1
+          }
+      }
+    ];
+  return r.aggregate(query).toArray();
+}
+
+function q7(r){
     let query = [
         {"$sort": {"pop": -1}},
         {"$project": {_id: 1, city: 1, state: 1}},
         {"$limit": 10}
     ];
-    r.aggregate(query).toArray(function(err, result){
-        if(err) throw err;
-        console.log(result);
-        client.close();
-    });
+    return r.aggregate(query).toArray();
 }
 
 function q8(r, client){
@@ -204,11 +224,7 @@ function q8(r, client){
         },
         {"$project": {city: 1, state: 1, _id: 0}}
     ];
-    r.aggregate(query).toArray(function(err, result){
-        if(err) throw err;
-        console.log(result);
-        client.close();
-    });
+    return r.aggregate(query).toArray();
 }
 
 function q9(r, client){
@@ -231,16 +247,12 @@ function q9(r, client){
             state: 1, totalPop: 1, _id: 0
         }}
     ];
-    r.aggregate(query).toArray(function(err, result){
-        if(err) throw err;
-        console.log(result);
-        client.close();
-    });
+    return r.aggregate(query).toArray();
 }
 
 function q10(r, client){
     r.createIndex({loc:"2dsphere"});
-    r.find({
+    return r.find({
         loc:
           { $near :
              {
@@ -250,21 +262,5 @@ function q10(r, client){
           }
       }, {projection: {_id: 0, city: 1}})
       .limit(5)
-      .toArray(function(err, result){
-        if(err) throw err;
-        console.log(result);
-        client.close();
-    });
+      .toArray();
 }
-
-// Execution
-queryFn("test", "restaurants", q1);
-queryFn("test", "restaurants", q2);
-queryFn("test", "restaurants", q3);
-queryFn("test", "restaurants", q4);
-queryFn("test", "restaurants", q5);
-queryFn("test", "restaurants", q6);
-queryFn("test", "zips", q7);
-queryFn("test", "zips", q8);
-queryFn("test", "zips", q9);
-queryFn("test", "zips", q10);
